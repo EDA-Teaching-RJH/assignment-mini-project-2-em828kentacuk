24c730f911f448e4cdfd90eab257486bc50f4c0f
em828kentacuk <em828@kent.ac.uk>
2026-03-10 21:38:12 +0000

    created the first class and added a function to add new items to the list.

--- project2.py
+++ project2.py
@@ -5,13 +5,30 @@ import random
 
 # imports all the functions from the other files
 
+class Barista:
+
+    def __init__(self, name, pay):
+        self.name = name
+        self.pay = pay
+        pay = 0
+        # initializes the Barista class with a name and pay, and sets pay to 0
+        if not name:
+            raise ValueError("Name cannot be empty")
+
+
+        
+
+
+
 def main():
 
     if len(sys.argv) < 2:
         print("Please enter an argument")
         sys.exit()
 # tells user to enter an argument if they don't and then exits the program
-    input = sys.argv[1]
+    
+input = sys.argv[1].lower()
+# takes the first argument and converts it to lowercase
 
 if __name__ == "__main__":
     main()
