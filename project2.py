e8985ec7f32e1d21e35ad7d6d4f43a83e5f2f70f
em828kentacuk <em828@kent.ac.uk>
2026-03-10 21:21:48 +0000

    started the main function first and created command line argumants using sys.argv

--- project2.py
+++ project2.py
@@ -3,9 +3,15 @@ import os
 import sys
 import random
 
+# imports all the functions from the other files
 
 def main():
-    pass
+
+    if len(sys.argv) < 2:
+        print("Please enter an argument")
+        sys.exit()
+# tells user to enter an argument if they don't and then exits the program
+    input = sys.argv[1]
 
 if __name__ == "__main__":
     main()
