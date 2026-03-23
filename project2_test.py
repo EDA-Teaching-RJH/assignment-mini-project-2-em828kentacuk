a0966a2e250fae15e1cc062b4713ea48fbf551d3
em828kentacuk <em828@kent.ac.uk>
2026-03-23 13:43:36 +0000

    list all our imports first, then we can add the test cases below

--- project2_test.py
+++ project2_test.py
@@ -1,3 +1,7 @@
-from project2 import Barista, save_barista
+import pytest
 
-def test_save_barista():
+from project2 import (
+    Barista, Novice, Intermediate, Expert, Manager, 
+    save_barista, load_baristas, save_manager, load_managers, assign_shifts
+)
+
